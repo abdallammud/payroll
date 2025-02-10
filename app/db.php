4824715cde9b6c191511573e413187ee6e3c3a73
abdallammud <abdallammud@gmail.com>
2025-02-10 08:34:40 +0300

    Marsuus

--- app/db.php
+++ app/db.php
@@ -5,10 +5,50 @@ $username   = "root";
 $password   = "";
 $db = "asheeri";*/
 
-$servername = "localhost";
-$username   = "u138037914_gamaas";
-$password   = ";9lZgpiFqxOA";
-$db = "u138037914_gamaas";
+function getSubdomain() {
+	// Get the current server name (e.g., "subdomain.example.com")
+	$server_name = $_SERVER['SERVER_NAME'];
+
+	// Extract the subdomain 
+	$parts = explode(".", $server_name);
+	$subdomain = array_shift($parts); // Remove and return the first element 
+
+	// Sanitize subdomain (optional)
+	return $sanitized_subdomain = preg_replace('/[^a-z0-9\-]/i', '', $subdomain); 
+}
+
+$subdomain = getSubdomain();
+
+
+// Define database credentials in an array
+$dbConfig = [
+    'default' => [
+        'host' => 'localhost',
+        'user' => 'root',
+        'password' => '',
+        'dbname' => 'asheeri'
+    ],
+    'gamaas' => [
+        'host' => 'localhost', 
+        'user' => 'u138037914_gamaas',
+        'password' => ';9lZgpiFqxOA',
+        'dbname' => 'u138037914_gamaas'
+    ],
+    'marsuus' => [
+        'host' => 'localhost', 
+        'user' => 'u138037914_marsuus', 
+        'password' => '/0zVtAUu', 
+        'dbname' => 'u138037914_marsuus'
+    ]
+];
+
+// Get the database configuration based on the subdomain
+$config = $dbConfig[$subdomain] ?? $dbConfig['default'];
+
+$servername = $config["host"];
+$username   = $config["user"];
+$password   = $config["password"];
+$db 		= $config["dbname"];
 
 $GLOBALS['conn'] = $conn = new mysqli($servername, $username, $password, $db);
 
